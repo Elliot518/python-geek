367fcb7f2c5b187acb10776d558a811d46d65451
Elliot <elliotgjy@163.com>
2024-06-24 15:24:39 +0800

    write doc

--- automation/word/write_word_doc.py
+++ automation/word/write_word_doc.py
@@ -1,5 +1,17 @@
 import docx
 
 doc = docx.Document()
+
+doc.add_heading('Header 0', 0)
+doc.add_heading('Header 1', 1)
+doc.add_heading('Header 2', 2)
+doc.add_heading('Header 3', 3)
+doc.add_heading('Header 4', 4)
+
 doc.add_paragraph('Hello, world!')
+paraObj1 = doc.add_paragraph('This is a second paragraph.')
+paraObj2 = doc.add_paragraph('This is a yet another paragraph.')
+paraObj1.add_run(' This text is being added to the second paragraph.')
 doc.save('helloworld.docx')
+
+print("doc saved")
